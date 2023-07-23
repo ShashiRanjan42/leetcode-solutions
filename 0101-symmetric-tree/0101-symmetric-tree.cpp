5d169aca48ed234a6ca1a8d8e2b7be03cecf8e0b
ShashiRanjan42 <101126472+ShashiRanjan42@users.noreply.github.com>
2023-07-23 18:33:07 +0530

    Time: 3 ms (87.53%), Space: 16.5 MB (30.57%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -13,36 +13,56 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         //By queue traversal Method
-        if(root == nullptr)
+        // if(root == nullptr)
+        // return true;
+        // TreeNode* left;
+        // TreeNode* right;
+
+        // queue<TreeNode*> q1,q2;
+        // q1.push(root->left);
+        // q2.push(root->right);
+
+        // while(!q1.empty() && !q2.empty())
+        // {
+        //     left = q1.front();
+        //     right = q2.front();
+        //     q1.pop();
+        //     q2.pop();
+
+        //     if(left == NULL && right == NULL)
+        //     continue;
+        //     if(left == NULL || right == NULL)
+        //     return false;
+
+        //     if(left->val != right->val)
+        //     return false;
+
+        //     q1.push(left->left);
+        //     q1.push(left->right);
+        //     q2.push(right->right);
+        //     q2.push(right->left);
+        // }
+        // return true;
+
+        // By Recursively
+        if(root == NULL)
         return true;
-        TreeNode* left;
-        TreeNode* right;
 
-        queue<TreeNode*> q1,q2;
-        q1.push(root->left);
-        q2.push(root->right);
+        return helper(root->left, root->right);
 
-        while(!q1.empty() && !q2.empty())
-        {
-            left = q1.front();
-            right = q2.front();
-            q1.pop();
-            q2.pop();
+    }
 
-            if(left == NULL && right == NULL)
-            continue;
-            if(left == NULL || right == NULL)
+    bool helper(TreeNode* p, TreeNode* q) {
+        if (p == nullptr && q == nullptr) {
+            return true;
+        } else if (p == nullptr || q == nullptr) {
             return false;
-
-            if(left->val != right->val)
-            return false;
-
-            q1.push(left->left);
-            q1.push(left->right);
-            q2.push(right->right);
-            q2.push(right->left);
         }
-        return true;
-
+        
+        if (p->val != q->val) {
+            return false;
+        }
+        
+        return helper(p->left,q->right) && helper(p->right, q->left); 
     }
 };
